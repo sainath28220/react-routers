65617d65b6b34def879c453e1db30c98beeb0405
sainath <sainathpabbathi22@gmail.com>
2021-12-30 14:03:35 +0530

    code wrtten

---
+++ Assignment-4/code/src/openpage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./openpage.css"
+
+
+const openpage = ()=>{
+    return(
+        <div>
+            <div>
+                <img src= "https://cdn.pixabay.com/photo/2016/11/29/03/44/animal-1867125__340.jpg" alt = "none"/>
+            </div>
+            <div>
+                <h1>Penguine helping people</h1>
+                <button>log in</button>
+            </div>
+        </div>
+    )
+}
+
+export default openpage
